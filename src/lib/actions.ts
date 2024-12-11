efb3891b922aecfa42667838bee3c85e818cad8f
Valexr <valexr@gmail.com>
2024-12-11 17:59:41 +0200

    test

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -19,8 +19,8 @@ export function resize(node: HTMLElement) {
 }
 
 export function expand(node: HTMLTextAreaElement) {
-    node.style.height = 'auto';
-    node.style.height = `${node.scrollHeight}px`;
+    // node.style.height = 'auto';
+    // node.style.height = `${node.scrollHeight}px`;
     node.oninput = (_e) => {
         node.style.height = 'auto';
         node.style.height = `${node.scrollHeight}px`;
