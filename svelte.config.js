a8b628a7a64898eddf70d7a42cbc3d586f58556e
Valexr <valexr@gmail.com>
2024-12-12 22:08:25 +0200

    fix prerender

--- svelte.config.js
+++ svelte.config.js
@@ -11,15 +11,9 @@ export default {
         // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
         // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
         // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-        adapter: adapter({
-            fallback: 'index.html',
-            pages: 'build/'
-        }),
+        adapter: adapter(),
         paths: {
             base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-        },
-        prerender: {
-            handleHttpError: 'warn'
         }
     }
 };
